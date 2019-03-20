567ac981c52fe95072b1c7820129b4de94494985
Sujit <sujitr@google.com>
2019-03-19 17:51:46 -0700

    refactoring

--- src/helloworld/index.js
+++ src/helloworld/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', async (req, res) => {
-  let frontendMessage = `Hello Node World!`;
-  res.send(`${frontendMessage}`);
+  res.send('Hello Node World!');
 })
 
 app.listen(8080, () => console.log('Hello World Nodejs listening on port 8080!'))
